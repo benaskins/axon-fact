48b45248108c250b8bf8e80230d4c4af3d2c541b
Ben Askins <human@getlamina.ai>
2026-04-05 15:18:22 +1000

    docs: update migration comment to reference axon-base

--- migrations.go
+++ migrations.go
@@ -3,7 +3,7 @@ package fact
 import "embed"
 
 // Migrations contains the SQL migration files for the events table.
-// Composition roots pass this to axon.RunMigrations or axon.MustRunMigrations.
+// Composition roots pass this to migration.Run from axon-base.
 //
 //go:embed migrations/*.sql
 var Migrations embed.FS
